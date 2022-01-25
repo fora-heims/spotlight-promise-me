f7e5ce81f6c1e568dd3ac77102916ac01a361ea1
fora-heims <foraheims@gmail.com>
2022-01-25 10:59:21 -0800

    First two fetch calls written.

--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,11 +1,20 @@
 /**
  * TODO:
  */
-
+const getNameInfo = async () => {
+  const response = await fetch('https://api.genderize.io/?name=fora');
+  const data = await response.json();
+  console.log(['async/await', data]);
+};
 /**
  * TODO:
  */
-
+const getNameInfoThen = () => {
+  const data = fetch('https://api.genderize.io/?name=fora').then((response) =>
+    response.json()
+  );
+  console.log(['.then()', data]);
+};
 /**
  * TODO:
  */
